12bfe009a16f028746b4ec31cd1a827714ca9e4b
Rudy-Lau <rudylauu@gmail.com>
2022-01-27 14:41:32 -0300

    PRACTICA vector ejercicio 2

--- Practica C#/Arrays/Ejercicios-Vectores01/Ejercicios-Vectores/Program.cs
+++ Practica C#/Arrays/Ejercicios-Vectores01/Ejercicios-Vectores/Program.cs
@@ -60,6 +60,24 @@ namespace Ejercicios_Vectores
             Console.WriteLine("EL MAYOR NUMERO ES: "+ maximo);
             Console.WriteLine("La cantidad de veces que se repite el 20 es: "+ contador);
             Console.ReadKey();*/
+
+            //EJERCICIO 2
+            /*Dado el vector inflación {0.8, 0.1, 0.3, 0.4, 0.3, 
+            0.6, 0.5, 0.3, 0.7, 0.3, 0.2, 0.9}
+            Cada ítem del vector representa la inflación de un mes, de tal manera que el primer ítem
+            del vector, que es 0.8, representa la inflación de Enero y el último ítem del vector, 
+            que es 0.9, representa la inflación de diciembre.
+            Se pide:
+            1. Informar la inflación anual
+            2. Informar la inflación más baja, junto con el 
+            número de mes. Por ejemplo: Mes 2 = 0.1
+            3. Informar la inflación más alta, junto con el 
+            número de mes. Por ejemplo: Mes 12 = 0.9
+            4. Informar el promedio de inflación (inflación 
+            total / 2)
+             */
+
+             
         }
     }
 }
